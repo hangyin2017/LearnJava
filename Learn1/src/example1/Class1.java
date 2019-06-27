56bb10a05c152cb89603d8b6ba744c1766206c67
Hang <hang.yin2017@outlook.com>
2019-06-27 20:53:50 +0930

    remove2

--- Learn1/src/example1/Class1.java
+++ Learn1/src/example1/Class1.java
@@ -7,8 +7,8 @@ public class Class1 {
 */
 	public static void main(String[] args) {
 		Solution solution1 = new Solution();
-		int[] nums = {2,7,11,17};
-		int target = 18;
+		int[] nums = {3,2,4};
+		int target = 6;
 		int[] result = solution1.twoSum(nums, target);
 		for(int temp : result) {
 			System.out.print(temp + " ");
