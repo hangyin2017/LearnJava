3392154d4d0c99a985016bcbd4ba61e97b3fc7f0
Hang <hang.yin2017@outlook.com>
2019-06-27 17:57:24 +0930

    solution

---
+++ Learn1/src/example1/Solution.java
@@ -0,0 +1,15 @@
+package example1;
+
+public class Solution {
+	public int[] twoSum(int[] nums, int target) {
+		for(int i = 0; i < (nums.length -1); i++) {
+			for(int j = 0; j < nums.length; j++) {
+				if(nums[j] == target - nums[i]) {
+					return new int[] {i,j};
+				}
+			}
+		}
+		return null;
+	}
+
+}
