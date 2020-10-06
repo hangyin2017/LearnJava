27428e1c56c8ef276831cb56f955c641041ee674
Hang <hang.yin2017@outlook.com>
2020-10-06 21:30:14 +1100

    ExampleCalendar: Can print month calendar.

--- Calendar/src/main/java/ExampleCalendar.java
+++ Calendar/src/main/java/ExampleCalendar.java
@@ -11,20 +11,64 @@ import java.util.Locale;
  * 2020/10/06
  */
 public class ExampleCalendar {
+
     public static void main(String[] args) {
-        GregorianCalendar d = new GregorianCalendar();
-        int firstDayOfWeek = d.getFirstDayOfWeek();
+
+        Locale.setDefault(Locale.US);
+
+        Calendar date = new GregorianCalendar();
+
         printShortWeekdays(getShortWeekdays());
+        printMonthDays(date);
+
     }
 
-    private static String[] getShortWeekdays() {
-        return new DateFormatSymbols(Locale.US).getShortWeekdays();
+    public static String[] getShortWeekdays() {
+        return new DateFormatSymbols().getShortWeekdays();
     }
 
-    private static void printShortWeekdays(String[] shortWeekDays) {
+    public static void printShortWeekdays(String[] shortWeekDays) {
+
         for (String shortWeekDay : shortWeekDays) {
+            if(!shortWeekDay.equals(""))
             System.out.printf("%4s", shortWeekDay);
         }
         System.out.println();
+
+    }
+
+    public static void printMonthDays(Calendar date) {
+
+        int today = date.get(Calendar.DAY_OF_MONTH);
+        int month = date.get(Calendar.MONTH);
+
+        date.set(Calendar.DAY_OF_MONTH, 1);
+        int weekday = date.get(Calendar.DAY_OF_WEEK);
+        int indent = 0;
+        int firstDayOfWeek = date.getFirstDayOfWeek();
+
+        while (weekday != firstDayOfWeek) {
+            indent++;
+            date.add(Calendar.DAY_OF_MONTH, -1);
+            weekday = date.get(Calendar.DAY_OF_WEEK);
+        }
+
+        for (; indent > 0; indent--) {
+            System.out.printf("%4s", " ");
+            date.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        date.set(Calendar.DAY_OF_WEEK, firstDayOfWeek);
+        date.add(Calendar.DAY_OF_WEEK, -1);
+        int lastDayOfWeek = date.get(Calendar.DAY_OF_WEEK);
+        date.set(Calendar.MONTH, month);
+        date.set(Calendar.DAY_OF_MONTH, 1);
+        for (; date.get(Calendar.MONTH) == month; date.add(Calendar.DAY_OF_MONTH, 1)) {
+            System.out.printf("%3d", date.get(Calendar.DAY_OF_MONTH));
+            if (date.get(Calendar.DAY_OF_MONTH) == today) System.out.print("*");
+            else System.out.print(" ");
+            if (date.get(Calendar.DAY_OF_WEEK) == lastDayOfWeek) System.out.println();
+        }
+
     }
 }
--- Calendar/src/test/java/ExampleCalendarTest.java
+++ Calendar/src/test/java/ExampleCalendarTest.java
@@ -1,5 +1,21 @@
+import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class ExampleCalendarTest {
+
+    @Test
+    public void testPrintShortWeekdays() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        ExampleCalendar.printShortWeekdays(ExampleCalendar.getShortWeekdays());
+
+        System.setOut(null);
+
+        assertEquals(" Sun Mon Tue Wed Thu Fri Sat\r\n", outContent.toString());
+    }
 
 }
